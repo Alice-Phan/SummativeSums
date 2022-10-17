0d4513120775d8dc5ef79298fe6ec860d3b440a5
Alice-Phan <alicebe.phan@gmail.com>
2022-10-16 21:57:23 -0500

    Declare and initialize variables, call new method to calculate the sums

--- src/SummativeSum.java
+++ src/SummativeSum.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class SummativeSum {
+
+public class SummativeSum {
+    public static void main (String[] args) {
+        // Declare and Initialize array variables
+        int[] array1 = { 1, 90, -33, -55, 67, -16, 28, -55, 15};
+        int[] array2 = { 999, -60, -77, 14, 160, 301 };
+        int[] array3 = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140,
+        150, 160, 170, 180, 190, 200, -99};
+
+        System.out.println("#1 Array Sum: " + arraySum(array1));
+        System.out.println("#2 Array Sum: " + arraySum(array2));
+        System.out.println("#3 Array Sum: " + arraySum(array3));
+
+    }
+    // Create and call new method to calculate the sum
+    public static int arraySum(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum;
+    }
+
 }
